ffc19d73dad7da0420e0f1ef891224f476db3e0f
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2025-11-06 07:48:11 +0100

    added database port
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- public/db-config.php
+++ public/db-config.php
@@ -4,5 +4,6 @@ $conn = ['host' => $_ENV['DB_HOST'],
     'user' => $_ENV['DB_USER'],
     'password' => $_ENV['DB_PASSWORD'],
     'dbname' => $_ENV['DB_NAME'],
+    'port' => $_ENV['DB_PORT'],
 ];
 
